cbb876cb3864336395a16da22ba154aa982ca58a
Your Name <you@example.com>
2022-01-19 18:18:06 +0100

    absolute path might not work on ~/nix as /nix

--- pkgs/development/compilers/rust/cargo.nix
+++ pkgs/development/compilers/rust/cargo.nix
@@ -23,8 +23,7 @@ rustPlatform.buildRustPackage {
       ref = "master";
       rev = "0a1dee55b28e5dd56ccc836cf919e62fedd7489a";
   }; in ''
-    ls -la
-    cp -r ${vendor-src} vendor/
+    cp -r ${vendor-src} source/vendor
   '';
 
   # changes hash of vendor directory otherwise
